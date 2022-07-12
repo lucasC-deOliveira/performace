16501b873bea623d4a27e5080a8d39b1aab42030
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-07-11 22:27:38 -0300

    useMemo

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,5 +1,5 @@
 import { ProductItem } from "./ProductItem";
-
+import { useMemo } from "react"
 
 type searchResultsProps = {
   results: Array<{
@@ -10,6 +10,26 @@ type searchResultsProps = {
 }
 
 export function SearchResults({ results }: searchResultsProps) {
+
+  //use memo memoriza um calculo
+
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price
+    }, 0)
+  }, [results]) // ultimo parametro é um array de dependencias
+
+  //exemplo quero que o calculo seja refeito sempre que results mudar
+
+
+  /*
+  APLICAÇÃO USE MEMO
+  
+  calculos pesados
+  igualdade referncial (passar informação a um componente filho)
+  */
+
+
   return (
     <div>
       {results.map(product => {
