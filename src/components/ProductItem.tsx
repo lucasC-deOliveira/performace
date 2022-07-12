84b346ca5a65d3b91e3cf43f60c8ec65feb84e53
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-07-11 22:17:41 -0300

    Memo

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react"
+
 
 interface ProductItemProps {
   product: {
@@ -9,7 +11,7 @@ interface ProductItemProps {
 }
 
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
 
   return (
     <div>
@@ -18,3 +20,17 @@ export function ProductItem({ product }: ProductItemProps) {
   )
 
 }
+
+//memo evita que o componente seja renderizado novamente quando passar por essa função
+
+/* 
+CASOS DE APLICAÇÃO
+
+Pure Functional Components
+Renders too often
+Re-renders with same props
+Medium to Big size Components
+*/
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+})
